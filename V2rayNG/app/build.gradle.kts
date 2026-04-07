f1cc5dbbe004df71016083cf11e5198652f7f87a
zarazaex69 <zarazaex@tuta.io>
2026-04-07 23:28:27 +0300

    chore(build): update application namespace and ID to com.olcng.app

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.v2ray.ang"
+    namespace = "com.olcng.app"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.olcng.app"
         minSdk = 24
         targetSdk = 36
         versionCode = 717
